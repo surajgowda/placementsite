1fed745ca79633ae76e43056fa3b2d8486b02d26
surajgowda <surajnca1234@gmail.com>
2023-08-17 07:16:54 +0000

    mistake

--- display/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Company, Updates, Notifications
-# Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(Company)
-admin.site.register(Updates)
-admin.site.register(Notifications)
